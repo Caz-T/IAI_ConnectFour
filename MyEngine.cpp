d35df1c659f1b6c2b29ec65e151fa5320a08b6ea
Casora Zitora <cqz21@mails.tsinghua.edu.cn>
2023-06-03 14:40:20 +0800

    feat: set kanarazu to 3

--- MyEngine.cpp
+++ MyEngine.cpp
@@ -197,7 +197,7 @@ Point* MyEngine::search(const int last_x, const int last_y, time_t ponder_limit)
     }
     // cerr << "Initialised / Stepped forward memory" << endl;
     node* to_ret = nullptr;
-    int kanarazu = ikanakerebanaranai(2);
+    int kanarazu = ikanakerebanaranai(3);
     if (kanarazu != -1) {
         while (memory->children[kanarazu] == nullptr) {
             auto vl = tree_policy(memory);
